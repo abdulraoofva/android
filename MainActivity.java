06d0d334f2e245ad6977ed6369c7a33fa94d8392
abdulraoofva <117900345+abdulraoofva@users.noreply.github.com>
2023-12-13 08:19:10 +0530

    exception

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,31 @@
-package com.example.intent;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+package com.example.execptions;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    EditText name;
-    EditText age;
-    @Override
+    List <String> list=new ArrayList<>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=findViewById(R.id.editText1);
-        age=findViewById(R.id.editText2);
-    }
-    public void switchActivity(View view){
-        Intent intent=new Intent(this,MainActivity2.class);
-        intent.putExtra("user",name.getText().toString());
-        intent.putExtra("age",age.getText().toString());
-        startActivity(intent);
-    }}
+        list.add("item1");
+        list.add("item2");
+        list.add("item3");
+        list.add("item4");
+        for (int i=0;i<5;i++){
+            try {
+                list.get(i);
 
+            } catch (Exception e) {
+                Toast.makeText(this,"rxception caught",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    }
