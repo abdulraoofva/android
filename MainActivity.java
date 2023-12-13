da719c721804b29c8285be8aafd827d145929779
abdulraoofva <117900345+abdulraoofva@users.noreply.github.com>
2023-12-13 08:15:38 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,78 +1,97 @@
-package com.example.calculator;
+package com.example.crud;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.textview.MaterialTextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-//Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16;
-    String currentInput = "";
-    double operand1 = 0;
-    String operator = "";
-MaterialTextView text;
-    @SuppressLint("WrongViewCast")
+EditText edit1,edit2,edit3;
+Button insert,read,update,delete;
+String rno,name,dept;
+SQLiteDatabase db;
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.t1);
-
+        edit1=findViewById(R.id.edit1);
+        edit2=findViewById(R.id.edit2);
+        edit3=findViewById(R.id.edit3);
+        insert=findViewById(R.id.insert);
+        read=findViewById(R.id.read);
+        update=findViewById(R.id.update);
+        delete=findViewById(R.id.delete);
+        DBHelper dbHelper= new DBHelper(this);
+        db = dbHelper.getReadableDatabase();
+        db=dbHelper.getWritableDatabase();
+    }
+    public void onInsert(View view){
+        rno=edit2.getText().toString();
+        name=edit1.getText().toString();
+        dept=edit3.getText().toString();
+        if (rno.equals("")||name.equals("")||dept.equals("")){
+            Toast.makeText(this,"enter the contents",Toast.LENGTH_LONG).show();
+        }
+        else {
+            ContentValues values=new ContentValues();
+            values.put("rollno",rno);
+            values.put("name",name);
+            values.put("dept",dept);
+            db.insert("student",null,values);
+            Toast.makeText(this,"inserted",Toast.LENGTH_LONG).show();
+        }
 
     }
-
-    public void onDigitClick(View view){
-        currentInput+=((Button)view).getText().toString();
-        updateDisplay();
-    }
-
-    private void updateDisplay() {
-        text.setText(currentInput);
-    }
-    public  void onOperator(View view){
-        if(!currentInput.isEmpty()) {
-            operand1=Double.parseDouble(currentInput);
-            operator = ((Button) view).getText().toString();
-            currentInput="";
+    public void onUpdate(View view){
+        rno=edit2.getText().toString();
+        name=edit1.getText().toString();
+        dept=edit3.getText().toString();
+        if (rno.equals("")||name.equals("")||dept.equals("")){
+            Toast.makeText(this,"no contents to display",Toast.LENGTH_LONG).show();
+        }
+        else {
+            ContentValues values=new ContentValues();
+            values.put("rollno",rno);
+            values.put("name",name);
+            values.put("dept",dept);
+            db.update("student",values,"rollno="+rno,null);
+            Toast.makeText(this,"values",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Updated",Toast.LENGTH_LONG).show();
         }
     }
-    public  void Equalclick(View view){
-        if(!currentInput.isEmpty()){
-            double operand2=Double.parseDouble(currentInput);
-            double res=operate(operand1,operator,operand2);
-            currentInput=String.valueOf(res);
-            updateDisplay();
+
+    public  void onDelete(View view){
+        rno=edit2.getText().toString();
+        name=edit1.getText().toString();
+        dept=edit3.getText().toString();
+        if (rno.equals("")||name.equals("")||dept.equals("")){
+            Toast.makeText(this,"no contents to display",Toast.LENGTH_LONG).show();
+        }
+        else {
+            db.delete("student","rollno="+rno,null);
+            Toast.makeText(this,"deleted",Toast.LENGTH_LONG).show();
         }
     }
-    public  void clear(View view){
-       operand1=0;
-       currentInput="";
-       operator="";
-       updateDisplay();
 
+    public  void onRead(View view){
+        StringBuffer buffer=new StringBuffer();
+        Cursor c=db.rawQuery("select * from student",null);
+        while (c.moveToNext()){
+            buffer.append("\n"+c.getString(0));
+            buffer.append("\n"+c.getString(1));
+            buffer.append("\n"+c.getString(2));
+        }
+        Toast.makeText(this,buffer.toString(),Toast.LENGTH_LONG).show();
     }
 
-    private double operate(double operand1, String operator, double operand2) {
-      switch (operator){
-          case "+":
-              return operand1 + operand2;
-          case "-":
-              return operand1 - operand2;
-          case "*":
-              return operand1 * operand2;
-          case "/":
-              if(operand2!=0){
-                  return operand1/operand2;
-              }else {
-                  return Double.NaN;
-              }
-          default:
-              return 0;
-      }
-    }
 }
+
