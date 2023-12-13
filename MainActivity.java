f99d951c9f83ea7fbe2bc14f0764a4e54dea2648
abdulraoofva <117900345+abdulraoofva@users.noreply.github.com>
2023-12-13 08:19:37 +0530

    listview

--- MainActivity.java
+++ MainActivity.java
@@ -1,31 +1,36 @@
-package com.example.execptions;
-
-import android.os.Bundle;
-import android.widget.Toast;
+package com.example.lists;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    List <String> list=new ArrayList<>();
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView lview;
+    String [] days={"sun","mon","tue","wed","thu","fri","sat"};
+    @SuppressLint("WrongViewCast")
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list.add("item1");
-        list.add("item2");
-        list.add("item3");
-        list.add("item4");
-        for (int i=0;i<5;i++){
-            try {
-                list.get(i);
+        lview=findViewById(R.id.listview);
+        ArrayAdapter<String>adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,days);
+        lview.setAdapter(adapter);
+        lview.setOnItemClickListener(this);
 
-            } catch (Exception e) {
-                Toast.makeText(this,"rxception caught",Toast.LENGTH_LONG).show();
-            }
-        }
+
+
 
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String clicked=days[i];
+        Toast.makeText(this,"you clicked"+clicked+"now",Toast.LENGTH_LONG).show();
     }
+}
