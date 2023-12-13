ec7803538e82abe5191702f38610670e4ba737cd
abdulraoofva <117900345+abdulraoofva@users.noreply.github.com>
2023-12-13 08:18:42 +0530

    intent

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,23 @@
-package com.example.spinner;
-
+package com.example.intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-    String[] names = {"values1", "values2", "values3", "value4", "value5"};
-
+    EditText name;
+    EditText age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner spinner = findViewById(R.id.sp1);
-        TextView textView = findViewById(R.id.t1);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                textView.setText(names[i]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+        name=findViewById(R.id.editText1);
+        age=findViewById(R.id.editText2);
     }
-}
+    public void switchActivity(View view){
+        Intent intent=new Intent(this,MainActivity2.class);
+        intent.putExtra("user",name.getText().toString());
+        intent.putExtra("age",age.getText().toString());
+        startActivity(intent);
+    }}
 
